0ba38ab790a30f77401010e808f76cd2d2dd723f
Atharva Kulkarni <Atharvak1910@gmail.com>
2024-11-21 21:47:56 +0530

    Adding points and vectors

--- src/main.zig
+++ src/main.zig
@@ -21,32 +21,26 @@ const tuple = struct {
             are_floats_equal(t1.z, t2.z, epsilon) or
             (t1.w == t2.w);
     }
+
+    fn add_vector(point: tuple, vector: tuple) tuple {
+        return tuple{
+            .x = point.x + vector.x,
+            .y = point.y + vector.y,
+            .z = point.z + vector.z,
+            .w = 1,
+        };
+    }
 };
 
 fn are_floats_equal(x: f64, y: f64, epsilon: comptime_float) bool {
     return std.math.complex.abs(x - y) < epsilon;
 }
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
-}
-
 test "simple test" {
     const pt1 = tuple.create_point(4.3, -4.2, 3.1);
-    const vec1 = tuple.create_vector(4.3, -4.2, 3.1);
-
+    const vec1 = tuple.create_vector(-4.3, 4.2, -3.1);
+    const pt2 = tuple.add_vector(pt1, vec1);
     try std.testing.expect(pt1.w == 1);
     try std.testing.expect(vec1.w == 0);
+    try std.testing.expect(pt2.x == 0 and pt2.y == 0 and pt2.z == 0 and pt2.w == 1);
 }
