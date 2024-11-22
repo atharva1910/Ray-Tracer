12f5061a56cbce476a7c83ba5725187eb6f01767
Atharva Kulkarni <atharvak1910@gmail.com>
2024-11-22 10:49:48 +0530

    Substract tuple

--- src/main.zig
+++ src/main.zig
@@ -31,6 +31,16 @@ const tuple = struct {
             .w = tup1.w + tup2.w,
         };
     }
+
+    fn sub_tuples(tup1: tuple, tup2: tuple) tuple {
+        std.debug.assert(tup1.w - tup2.w == 1 or tup1.w - tup2.w == 0);
+        return tuple{
+            .x = tup1.x - tup2.x,
+            .y = tup1.y - tup2.y,
+            .z = tup1.z - tup2.z,
+            .w = tup1.w - tup2.w,
+        };
+    }
 };
 
 fn are_floats_equal(x: f64, y: f64, epsilon: comptime_float) bool {
@@ -40,8 +50,14 @@ fn are_floats_equal(x: f64, y: f64, epsilon: comptime_float) bool {
 test "simple test" {
     const pt1 = tuple.create_point(4.3, -4.2, 3.1);
     const vec1 = tuple.create_vector(-4.3, 4.2, -3.1);
-    const pt2 = tuple.add_tuples(pt1, vec1);
+    const vec2 = tuple.create_vector(-4.3, 4.2, -3.1);
     try std.testing.expect(pt1.w == 1);
     try std.testing.expect(vec1.w == 0);
+
+    const pt2 = tuple.add_tuples(pt1, vec1);
+    const pt3 = tuple.sub_tuples(pt1, vec1);
+    const vec3 = tuple.sub_tuples(vec2, vec1);
     try std.testing.expect(pt2.x == 0 and pt2.y == 0 and pt2.z == 0 and pt2.w == 1);
+    try std.testing.expect(pt3.w == 1);
+    try std.testing.expect(vec3.w == 0);
 }
