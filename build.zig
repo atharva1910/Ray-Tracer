b3ad32d0af216197165a8e96f2b94d374a479bd5
Atharva Kulkarni <atharvak1910@gmail.com>
2024-11-24 17:26:18 +0530

    Create canvas and modify build.zig

--- build.zig
+++ build.zig
@@ -38,7 +38,15 @@ pub fn build(b: *std.Build) void {
     });
     const run_tuple_unit_tests = b.addRunArtifact(tuple_unit_tests);
 
+    const canvas_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/canvas.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const run_canvas_unit_tests = b.addRunArtifact(canvas_unit_tests);
+
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
     test_step.dependOn(&run_tuple_unit_tests.step);
+    test_step.dependOn(&run_canvas_unit_tests.step);
 }
---
+++ src/canvas.zig
@@ -0,0 +1,48 @@
+const std = @import("std");
+const tuple = @import("tuple.zig").tuple;
+
+pub const canvas = struct{
+    width: u32,
+    height: u32,
+    allocator: *const std.mem.Allocator,
+    pixels: [][]tuple,
+
+    pub fn init_canvas(allocator:  *const std.mem.Allocator, width: u32, height: u32) !canvas {
+        return canvas {
+            .width = width,
+            .height = height,
+            .allocator = allocator,
+            .pixels = try alloc_pixels(allocator, width, height),
+        };
+    }
+
+    fn alloc_pixels(allocator: *const std.mem.Allocator, width: u64, height: u64) ![][]tuple {
+        const arr_ptr = try allocator.alloc([]tuple, width);
+        for (arr_ptr, 0..) |_, i| {
+            arr_ptr[i] = try allocator.alloc(tuple, height);
+            for(arr_ptr[i], 0..) |_, j| {
+                arr_ptr[i][j] = tuple.create_color(0,0,0);
+            }
+
+        }
+
+        return arr_ptr;
+    }
+
+    pub fn free_canvas(self: *canvas) void {
+        const arr_ptr = self.pixels;
+        for (arr_ptr, 0..) |_, i| {
+            self.allocator.free(arr_ptr[i]);
+        }
+        self.allocator.free(arr_ptr);
+    }
+};
+
+test "canvas test" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    errdefer _ = gpa.deinit();
+
+    var window = try canvas.init_canvas(&gpa.allocator(), 10, 20);
+    defer window.free_canvas();
+    try std.testing.expect(window.width == 10 and window.height == 20);
+}
