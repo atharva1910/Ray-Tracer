224a2abfb782eb331f47fb5fb59f508697ec630c
Atharva Kulkarni <atharvak1910@gmail.com>
2024-11-28 21:59:07 +0530

    Some cleanup

--- src/main.zig
+++ src/main.zig
@@ -13,23 +13,44 @@ const game = struct {
         gravity: tuple,
         wind: tuple,
     };
+    const pixel_color = tuple.create_color(1, 0, 0);
 
     proj: projectile,
     env: environment,
+    window: canvas,
 
-    fn init(t1: tuple, t2: tuple, t3: tuple, t4: tuple) game {
-        return .{ .proj = .{
-            .position = t1,
-            .velocity = t2,
-        }, .env = .{
-            .gravity = t3,
-            .wind = t4,
-        } };
+    fn init(allocator: *const std.mem.Allocator, t1: tuple, t2: tuple, t3: tuple, t4: tuple) !game {
+        var gameState: game = undefined;
+        gameState.window = try canvas.init_canvas(allocator, 900, 900);
+        gameState.window.fill_all_pixels(tuple.create_color(0, 0, 0));
+
+        gameState.proj = .{ .position = t1, .velocity = t2 };
+        gameState.env = .{ .gravity = t3, .wind = t4 };
+
+        return gameState;
     }
 
-    fn tick(self: *game) void {
+    fn draw_pixel(self: *game) void {
+        const fheight: f64 = @floatFromInt(self.window.height);
+        self.window.write_pixel_scale(self.proj.position.x, fheight - self.proj.position.y, pixel_color);
+    }
+
+    fn tick(self: *game) bool {
         self.proj.position = tuple.add_tuples(self.proj.position, self.proj.velocity);
         self.proj.velocity = tuple.add_tuples(tuple.add_tuples(self.proj.velocity, self.env.gravity), self.env.wind);
+        draw_pixel(self);
+
+        if (self.proj.position.y < 0) {
+            self.window.save("game.ppm") catch |e| {
+                std.log.err("Failed to save ppm {}\n", .{e});
+            };
+            return false;
+        }
+        return true;
+    }
+
+    fn deinit(self: *game) void {
+        self.window.free_canvas();
     }
 };
 
@@ -37,25 +58,16 @@ pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     errdefer _ = gpa.deinit();
 
-    const window = try canvas.init_canvas(&gpa.allocator(), 900, 900);
-    defer window.free_canvas();
+    const position = tuple.create_point(0, 1, 0);
+    const velocity = tuple.normalize(tuple.create_vector(1, 1, 0));
+    const gravity = tuple.create_vector(0, -0.1, 0);
+    const wind = tuple.create_vector(-0.01, 0, 0);
 
-    const red = tuple.create_color(1, 0, 0);
-    const fheight: f64 = @floatFromInt(window.height);
-
-    window.fill_all_pixels(tuple.create_color(0, 0, 0));
-
-    var state = game.init(tuple.create_point(0, 1, 0), tuple.normalize(tuple.create_vector(1, 1, 0)), tuple.create_vector(0, -0.1, 0), tuple.create_vector(-0.01, 0, 0));
-
-    window.write_pixel_scale(state.proj.position.x, fheight - state.proj.position.y, red);
-
-    while (true) {
-        state.tick();
-        window.write_pixel_scale(state.proj.position.x, fheight - state.proj.position.y, red);
-        if (state.proj.position.y < 0) break;
-    }
-
-    window.save("game.ppm") catch |e| {
-        std.log.err("Failed to save ppm {}\n", .{e});
+    var state = game.init(&gpa.allocator(), position, velocity, gravity, wind) catch |err| {
+        std.log.err("Failed to init the game state {}\n", .{err});
+        return;
     };
+    defer state.deinit();
+
+    while (state.tick()) {}
 }
