86f554756a86c506a11eb6d938d87bc0a0faea95
Atharva Kulkarni <atharvak1910@gmail.com>
2024-11-30 08:42:11 +0530

    Same matrix init?

--- src/matrix.zig
+++ src/matrix.zig
@@ -1,26 +1,60 @@
 const std = @import("std");
 
 const matrix = struct {
-    m: [4][4]f64,
+    const MAX_SIZE = 4;
+    values: [MAX_SIZE][MAX_SIZE]f64,
+    row_size: u8,
+    col_size: u8,
 
-    fn init(m00: f64, m01: f64, m02: f64, m03: f64, m10: f64, m11: f64, m12: f64, m13: f64, m20: f64, m21: f64, m22: f64, m23: f64, m30: f64, m31: f64, m32: f64, m33: f64) matrix {
-        return matrix{ .m = .{ .{ m00, m01, m02, m03 }, .{ m10, m11, m12, m13 }, .{ m20, m21, m22, m23 }, .{ m30, m31, m32, m33 } } };
+    pub fn init(row_size: u8, col_size: u8) matrix {
+        std.debug.assert(row_size >= 2 and row_size <= MAX_SIZE);
+        std.debug.assert(col_size >= 2 and col_size <= MAX_SIZE);
+
+        return matrix{
+            .row_size = row_size,
+            .col_size = col_size,
+            .values = undefined,
+        };
     }
 
-    fn get_value_at(self: *const matrix, x: u8, y: u8) f64 {
-        std.debug.assert(x < 4 and y < 4);
-        return self.m[x][y];
+    pub fn set(self: *matrix, x: u8, y: u8, value: f64) void {
+        std.debug.assert(x < self.row_size and y < self.col_size);
+        self.values[x][y] = value;
+    }
+
+    pub fn get(self: *const matrix, x: u8, y: u8) f64 {
+        std.debug.assert(x < self.row_size and y < self.col_size);
+        return self.values[x][y];
     }
 };
 
 test "matrix test" {
-    const m1 = matrix.init(1, 2, 3, 4, 5.5, 6.5, 7.5, 8.5, 9, 10, 11, 12, 13.5, 14.5, 15.5, 16.5);
+    var m1 = matrix.init(4, 4);
+    m1.set(0, 0, 1);
+    m1.set(0, 1, 2);
+    m1.set(0, 2, 3);
+    m1.set(0, 3, 4);
 
-    try std.testing.expect(m1.get_value_at(0, 0) == 1);
-    try std.testing.expect(m1.get_value_at(0, 3) == 4);
-    try std.testing.expect(m1.get_value_at(1, 0) == 5.5);
-    try std.testing.expect(m1.get_value_at(1, 2) == 7.5);
-    try std.testing.expect(m1.get_value_at(2, 2) == 11);
-    try std.testing.expect(m1.get_value_at(3, 0) == 13.5);
-    try std.testing.expect(m1.get_value_at(3, 2) == 15.5);
+    m1.set(1, 0, 5.5);
+    m1.set(1, 1, 6.5);
+    m1.set(1, 2, 7.5);
+    m1.set(1, 3, 8.5);
+
+    m1.set(2, 0, 9);
+    m1.set(2, 1, 10);
+    m1.set(2, 2, 11);
+    m1.set(2, 3, 12);
+
+    m1.set(3, 0, 13.5);
+    m1.set(3, 1, 14.5);
+    m1.set(3, 2, 15.5);
+    m1.set(3, 3, 16.5);
+
+    try std.testing.expect(m1.get(0, 0) == 1);
+    try std.testing.expect(m1.get(0, 3) == 4);
+    try std.testing.expect(m1.get(1, 0) == 5.5);
+    try std.testing.expect(m1.get(1, 2) == 7.5);
+    try std.testing.expect(m1.get(2, 2) == 11);
+    try std.testing.expect(m1.get(3, 0) == 13.5);
+    try std.testing.expect(m1.get(3, 2) == 15.5);
 }
