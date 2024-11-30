bcfd56be959d3beb4df7bca82eda050af3926883
Atharva Kulkarni <atharvak1910@gmail.com>
2024-11-30 08:51:19 +0530

    row init

--- src/matrix.zig
+++ src/matrix.zig
@@ -22,6 +22,19 @@ const matrix = struct {
         self.values[x][y] = value;
     }
 
+    pub fn set_row(self: *matrix, row_num: u8, data: [4]f64) void {
+        std.debug.assert(row_num < self.row_size);
+        self.values[row_num] = data;
+    }
+
+    pub fn set_col(self: *matrix, col_num: u8, data: [4]f64) void {
+        std.debug.assert(col_num < self.col_size);
+        self.values[0][col_num] = data[0];
+        self.values[1][col_num] = data[1];
+        self.values[2][col_num] = data[2];
+        self.values[3][col_num] = data[3];
+    }
+
     pub fn get(self: *const matrix, x: u8, y: u8) f64 {
         std.debug.assert(x < self.row_size and y < self.col_size);
         return self.values[x][y];
@@ -30,25 +43,10 @@ const matrix = struct {
 
 test "matrix test" {
     var m1 = matrix.init(4, 4);
-    m1.set(0, 0, 1);
-    m1.set(0, 1, 2);
-    m1.set(0, 2, 3);
-    m1.set(0, 3, 4);
-
-    m1.set(1, 0, 5.5);
-    m1.set(1, 1, 6.5);
-    m1.set(1, 2, 7.5);
-    m1.set(1, 3, 8.5);
-
-    m1.set(2, 0, 9);
-    m1.set(2, 1, 10);
-    m1.set(2, 2, 11);
-    m1.set(2, 3, 12);
-
-    m1.set(3, 0, 13.5);
-    m1.set(3, 1, 14.5);
-    m1.set(3, 2, 15.5);
-    m1.set(3, 3, 16.5);
+    m1.set_row(0, .{ 1, 2, 3, 4 });
+    m1.set_row(1, .{ 5.5, 6.5, 7.5, 8.5 });
+    m1.set_row(2, .{ 9, 10, 11, 12 });
+    m1.set_row(3, .{ 13.5, 14.5, 15.5, 16.5 });
 
     try std.testing.expect(m1.get(0, 0) == 1);
     try std.testing.expect(m1.get(0, 3) == 4);
